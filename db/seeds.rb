a323ebd2a4c2bd8beb7a2e8977f1564010141443
Phuong Cao <caophuong2012@gmail.com>
2017-09-15 18:12:00 +0700

    [Issue #1] Create page Contact us
    - Add Contact us menu
    
    Signed-off-by: Phuong Cao <caophuong2012@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Contact.create!(address: "35 Nguyen Hue, district 1, Ho Chi Minh city", phone: " - 0933129591")
+Contact.create!(address: "16 Hai Trieu, district 1, Ho Chi Minh city", phone: " - 0933129591")
